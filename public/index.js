d5b4ca548c666c167af52424c9817294b34de0fa
nao-sph <naoki.nagai@icloud.com>
2016-10-01 12:52:04 +0900

    change functions, add descriptions

--- public/index.js
+++ public/index.js
@@ -1,37 +1,53 @@
 let roomList, isPlayMode;
 
-function display(){
+function display(){ // モードに合わせて部屋を表示する関数
   $('#displayList').empty();
-  if(isPlayMode == false){
-    $.get("/api/room/list", (data) => {
+  if(isPlayMode == false){ // 観戦モード
+    $.get("/api/room/list", (data) => { // roomListの取得、htmlに書き込み
       roomList = data.data;
       for(let val of roomList){
-        let html = `<button class="room notPlaying" onClick=clickedButton('${val.id}')>
-        <span class="mainInfo">${('00' + val.number).slice(-3)}</span><br>
-        <span class="addInfo">member ${val.population}/8</span>
-        </button>`;
-        $("#displayList").append(html);
-        $('body').removeClass("playing").addClass("notPlaying");
+        dispList(val, false);
       }
     });
-  } else {
+    $('body').removeClass("playing").addClass("notPlaying");
+    $('button[id=makeRoom]').remove();
+    $('#displayMode').html('観戦');
+    $('#changeMode').html('参戦モードへ');
+  } else { // 参戦モード
     isPlayMode = true;
-    $.get("/api/room/list", (data) => {
+    $.get("/api/room/list", (data) => { // roomListの取得、htmlに書き込み
       roomList = data.data;
       for(let val of roomList){
         if(val.population >= 8) continue;
-        let html = `<button class="room playing" onClick=clickedButton('${val.id}')>
-        <span class="mainInfo">${('00' + val.number).slice(-3)}</span><br>
-        <span class="addInfo">member ${val.population}/8</span>
-        </button>`;
-        $("#displayList").append(html);
-        $('body').removeClass("notPlaying").addClass("playing");
+        dispList(val, true);
       }
     });
+    $('body').removeClass("notPlaying").addClass("playing");
+    $('button[id=makeRoom]').remove();
+    $('#displayMode').html('参戦');
+    $('#changeMode').html('観戦モードへ');
+    // 部屋を作るボタンを作る
+    $(`<button id="makeRoom">
+      <span class="mainInfo">new Room</span>
+      </button>`).insertAfter('div[id=displayList]');
+    $("button[id=makeRoom]").click(() => { // 部屋を作るボタンの設定
+      $.post("/api/room/make", (data) => {
+        $.post("/api/room/" + data.data + "/connect", (data) => { // 何か投げる (予定)
+          console.log(data);
+        });
+      });
+    });
+  }
+  function dispList(val,isPlayMode){ // Listのvalをもとにhtmlに書き込む関数
+    let html = `<button class="room ${isPlayMode == false ? 'notPlaying' : 'playing'}" onClick=clickedButton('${val.id}')>
+    <span class="mainInfo">${('00' + val.number).slice(-3)}</span><br>
+    <span class="addInfo">member ${val.population}/8</span>
+    </button>`;
+    $("#displayList").append(html);
   }
 }
 
-function clickedButton(id){
+function clickedButton(id){ // clickされたButtonの内容を送る?関数 (予定)
   if(isPlayMode){
     $.post(`/api/room/${id}/connect`, (data) => {
       console.log(data);
@@ -43,31 +59,12 @@ function clickedButton(id){
   }
 }
 
-function changeMode(){
+function changeMode(){ // モードを変える
   if(isPlayMode == true){
     isPlayMode = false;
+    display();
   } else {
     isPlayMode = true;
-  }
-  if(isPlayMode == false){
     display();
-    $('#displayMode').html('観戦');
-    $('#changeMode').html('参戦モードへ');
-    $('button[id=makeRoom]').remove();
-  } else {
-    display();
-    $('#displayMode').html('参戦');
-    $('#changeMode').html('観戦モードへ');
-    $('button[id=makeRoom]').remove();
-    $(`<button id="makeRoom">
-      <span class="mainInfo">new Room</span>
-      </button>`).insertAfter('div[id=displayList]');
-    $("button[id=makeRoom]").click(() => {
-      $.post("/api/room/make", (data) => {
-        $.post("/api/room/" + data.data + "/connect", (data) => {
-          console.log(data);
-        });
-      });
-    });
   }
 }
